d4580070b601c16643486c9388f7b0906861fd02
lighthoof <andrew.kuligin@gmail.com>
2025-08-21 14:53:02 +0300

    Added CICD
    * Change to readme
    
    * Added basic CICD config
    
    * Removed forced failure step in CICD config
    
    * Added GetAPIKey test
    
    * Fixed the test to actually pass
    
    * Checking the CI
    
    * Reverted the code to pass the tests
    
    * Added coverga reporting to tests
    
    * Added test badge to readme

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := map[string]struct {
+		input http.Header
+		want  string
+	}{
+		"simple":   {input: http.Header{"Authorization": []string{"ApiKey asdafdsagdfgsdh"}}, want: "asdafdsagdfgsdh"},
+		"trailing": {input: http.Header{"Authorization": []string{"ApiKey asdafdsagdfgsdh asdfgdfhd asdgrdfgf sdfgafds"}}, want: "asdafdsagdfgsdh"},
+	}
+
+	//got, err := GetAPIKey(http.Header{"Authorization": []string{"asdafdsagdfgsdh"}})
+	//want := "ApiKey asdafdsagdfgsdh"
+
+	for name, tc := range tests {
+		got, err := GetAPIKey(tc.input)
+		if err != nil {
+			t.Fatalf("%v: Error executing GetAPIKey function : %v", name, err)
+		}
+		diff := cmp.Diff(tc.want, got)
+		if diff != "" {
+			t.Fatalf("%v: %v", name, diff)
+		}
+	}
+
+}
